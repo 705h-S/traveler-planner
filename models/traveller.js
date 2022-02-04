05ef264c879890b8397111300e00be399975b561
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-04 12:16:39 -0800

    created traverler model

---
+++ models/traveller.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } =require('sequelize');
+const sequelize = require('../config/connection');
+
+class Traveller extends Model {}
+
+Traveller.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name:{
+            type: DataTypes.STRING,
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+            isEmail: true,
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Location',
+     },
+    },
+);
+
+module.exports = Traveller;
